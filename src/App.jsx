cb276489b751a09205cb32866910daf6a1115d98
Alsherif <122943516+Elsheerif@users.noreply.github.com>
2025-07-23 22:28:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ export default function App() {
   const router = createBrowserRouter([
     {
       path: "", element: < Layout />, children:[
+         {path:"", element:<Home/>},
         { path: "Home", element: <Home /> },
         { path: "About", element: <About /> },
         { path: "Portfolio", element: <Portfolio /> },
